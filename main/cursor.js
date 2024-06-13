5b199e17b67f2e90a1104b54becb1fe50ce22e7c
csafonsbrondes <csafonsbrondes@gmail.com>
2024-06-12 22:24:34 -0300

    TP2 ADD: Diseño pagina principal - provisorio

--- main/cursor.js
+++ main/cursor.js
@@ -1,11 +1,11 @@
 document.body.style.cursor = 'none'; // Oculta el cursor original
 
 const varita = document.createElement('img');
-varita.src = 'image/varita.png'; // Asegúrate de que la ruta a la imagen es correcta
+varita.src = 'image/varita.png'; // Ruta de la imagen de la varita
 varita.className = 'varita'; // Añade la clase CSS a la imagen
-document.body.appendChild(varita);
+document.body.appendChild(varita); // Añade la imagen al cuerpo del documento
 
 document.addEventListener('mousemove', (e) => {
-    varita.style.left = e.pageX + 'px';
-    varita.style.top = e.pageY + 'px';
+    varita.style.left = e.pageX + 'px'; // Establece la posición horizontal de la varita
+    varita.style.top = e.pageY + 'px'; // Establece la posición vertical de la varita
 });
